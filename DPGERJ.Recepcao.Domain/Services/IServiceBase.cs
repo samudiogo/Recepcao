906ec65b011e820d0b6750e2f9b32f5e268e275e
Sandro Arpi <sandroarpi72@gmail.com>
2016-11-03 17:29:44 -0200

    Criacao da classe DataContext e das interfaces de servico dentro do Dominio

---
+++ DPGERJ.Recepcao.Domain/Services/IServiceBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPGERJ.Recepcao.Domain.Services
+{
+    interface IServiceBase<TEntity> where TEntity : class
+    {
+        void Add(TEntity obj);
+        TEntity GetById(int id);
+        IEnumerable<TEntity> GetAll();
+        void Update(TEntity obj);
+        void Remove(TEntity obj);
+    }
+}
