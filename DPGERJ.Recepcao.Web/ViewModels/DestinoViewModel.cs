94d6e4f10928b9382f6b5ec4d9840b244a22745b
Sandro Arpi <sandroarpi72@gmail.com>
2016-11-10 16:27:43 -0200

    Criacao controller e ViewModel

---
+++ DPGERJ.Recepcao.Web/ViewModels/DestinoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DPGERJ.Recepcao.Web.ViewModels
+{
+    public class DestinoViewModel
+    {
+        public string Nome { get; set; }
+        public string Andar { get; set; }
+    }
+}
