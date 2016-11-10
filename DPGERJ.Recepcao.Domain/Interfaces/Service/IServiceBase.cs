4cd3e7aa779f794aea7bcf54b956c37d4a67bc0c
Supremo Samuel Diogo <samudiogo@gmail.com>
2016-11-10 16:05:10 -0200

    criação das interfaces e classes bases padrões

---
+++ DPGERJ.Recepcao.Domain/Interfaces/Service/IServiceBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace DPGERJ.Recepcao.Domain.Interfaces.Service
+{
+    public interface IServiceBase<TEntity> where TEntity : class
+    {
+        /// <summary>
+        ///  retorna o objeto que tenha um id idêntico ao mencionado pelo usuário
+        /// </summary>
+        /// <param name="id">id chave que identifica a entidade</param>
+        /// <returns></returns>
+        TEntity GetById(int id);
+        IEnumerable<TEntity> GetAll();
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Remove(TEntity entity);
+    }
+}
