5ba13bd8b7f06cea43f7e7a1479adfb424be2f0d
Sandro Arpi <sandroarpi72@gmail.com>
2016-11-10 18:01:45 -0200

    Interfaces e Serviços do projeto Application

---
+++ DPGERJ.Recepcao.Application/Services/AppServiceBase.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using DPGERJ.Recepcao.Application.Interfaces;
+using DPGERJ.Recepcao.Domain.Interfaces.Service;
+
+namespace DPGERJ.Recepcao.Application.Services
+{
+    public class AppServiceBase<TEntity> : IAppServiceBase<TEntity> where TEntity : class
+    {
+        protected IServiceBase<TEntity> Service { get; }
+
+        public AppServiceBase(IServiceBase<TEntity> service)
+        {
+            Service = service;
+        }
+
+        public void Add(TEntity entity) => Service.Add(entity);
+
+        public IEnumerable<TEntity> GetAll() => Service.GetAll();
+
+        public TEntity GetById(int id) => Service.GetById(id);
+
+        public void Remove(TEntity entity) => Service.Remove(entity);
+
+        public void Update(TEntity entity) => Service.Update(entity);
+    }
+}
---
+++ DPGERJ.Recepcao.Application/Services/VisitaAppService.cs
@@ -0,0 +1,13 @@
+﻿using DPGERJ.Recepcao.Application.Interfaces;
+using DPGERJ.Recepcao.Domain.Entities;
+using DPGERJ.Recepcao.Domain.Interfaces.Service;
+
+namespace DPGERJ.Recepcao.Application.Services
+{
+    public class VisitaAppService : AppServiceBase<Visita>, IVisitaAppService
+    {
+        public VisitaAppService(IServiceBase<Visita> service) : base(service)
+        {
+        }
+    }
+}
